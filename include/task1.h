9f83f6b472654e3a2739794d20c4b39e128c7f73
valvarl <valvarl@ya.ru>
2020-11-02 20:39:06 +0300

    msort fix

--- include/task1.h
+++ include/task1.h
@@ -16,7 +16,7 @@ bool comp(T a, T b){
 
 template<>
 bool comp<char *>(char *a, char *b){
-    return std::strlen(a) < std::strlen(b);
+    return strlen(a) < strlen(b);
 }
 
 template<typename T>
@@ -33,27 +33,27 @@ void msort(T arr[], int N) noexcept
         std::unique_ptr<T[]> tmp_array(new T[N]);
         while (li < l || ri < N) {
             if (comp(arr[li], arr[ri])) {
-                tmp_array[i++] = std::move(arr[li]);
+                tmp_array[i++] = move(arr[li]);
                 li++;
             } else {
-                tmp_array[i++] = std::move(arr[ri]);
+                tmp_array[i++] = move(arr[ri]);
                 ri++;
             }
             if (li == l) {
-                std::copy(std::make_move_iterator(&arr[ri]),
-                        std::make_move_iterator(&arr[N]),
+                copy(make_move_iterator(&arr[ri]),
+                        make_move_iterator(&arr[N]),
                         &tmp_array[i]);
                 break;
             }
             if (ri == N) {
-                std::copy(std::make_move_iterator(&arr[li]),
-                        std::make_move_iterator(&arr[l]),
+                copy(make_move_iterator(&arr[li]),
+                        make_move_iterator(&arr[l]),
                         &tmp_array[i]);
                 break;
             }
         }
-        std::copy(std::make_move_iterator(&tmp_array[0]),
-                std::make_move_iterator(&tmp_array[N]),
+        copy(make_move_iterator(&tmp_array[0]),
+                make_move_iterator(&tmp_array[N]),
                 &arr[0]);
     }
 }
