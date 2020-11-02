763a93305c020a9333c4b3ccc84e7d627ba0c146
Vlad <zagoskin.3@mail.ru>
2020-11-02 19:09:17 +0300

    1

--- src/main1.cpp
+++ src/main1.cpp
@@ -15,14 +15,18 @@ int comp(const std :: string a , const std::string b)
 int main()
 {
 	double a [] = {1.1,9.3,4.2,2.2,3.6,7.1,5.5,0};
-	msort(a,comp2);
+	msort(a);
 	for(int i = 0; i < 8; i++)
 	{
 		std :: cout << a[i] << std :: endl;
 	}
 	
-	std :: string b[] = {"aaa","a","aaaaaaaa","aaaa"};
-	msort(b,comp);
+	char**b = (char**)calloc(4,sizeof(char*));
+	b[0] = "aaa";
+	b[1] = "a";
+	b[2] = "aaaaaaaaa";
+	b[3] = "aaaa";
+	msort(b,4);
 	for(int i = 0; i < 4; i++)
 	{
 		std::cout << b[i] << std :: endl;
