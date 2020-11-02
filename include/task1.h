6bbccec4679bb576b8e0abc0eaf60d1f6fe6bc37
valvarl <valvarl@ya.ru>
2020-11-02 21:39:43 +0300

    msort fix

--- include/task1.h
+++ include/task1.h
@@ -34,28 +34,28 @@ void msort(T arr[], int N) noexcept
         T*  tmp_array = new T[N];
         while (li < l || ri < N) {
             if (comp(arr[li], arr[ri])) {
-                tmp_array[i++] = std::move(arr[li]);
+                tmp_array[i++] = arr[li];
                 li++;
             } else {
-                tmp_array[i++] = std::move(arr[ri]);
+                tmp_array[i++] = arr[ri];
                 ri++;
             }
             if (li == l) {
-                std::copy(std::make_move_iterator(&arr[ri]),
-                          std::make_move_iterator(&arr[N]),
-                        &tmp_array[i]);
+                for (size_t w = ri; w < N; w++){
+                    tmp_array[i+w-ri] = arr[w];
+                }
                 break;
             }
             if (ri == N) {
-                std::copy(std::make_move_iterator(&arr[li]),
-                          std::make_move_iterator(&arr[l]),
-                        &tmp_array[i]);
+                for (size_t w = li; w < l; w++){
+                    tmp_array[i+w-li] = arr[w];
+                }
                 break;
             }
         }
-        std::copy(std::make_move_iterator(&tmp_array[0]),
-                  std::make_move_iterator(&tmp_array[N]),
-                &arr[0]);
+        for (size_t w = 0; w < N; w++){
+            arr[w] = tmp_array[w];
+        }
     }
 }
 
