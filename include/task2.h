ac356954950335b0f87f38bf849d557f6811c539
KuznetsovMikhail <KuznetsovMikhail>
2020-11-02 21:04:40 +0300

    First try

---
+++ include/task2.h
@@ -0,0 +1,16 @@
+#ifndef TASK2_H_INCLUDED
+#define TASK2_H_INCLUDED
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+bool checkPrime(unsigned long long value);
+unsigned long long nPrime(unsigned n);
+unsigned long long nextPrime(unsigned long long value);
+
+
+#endif // TASK2_H_INCLUDED
---
+++ include/task3.h
@@ -0,0 +1,16 @@
+#ifndef TASK3_H_INCLUDED
+#define TASK3_H_INCLUDED
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+unsigned long long sumPrime(unsigned long long hbound);
+static bool checkPrimeNew(unsigned long long value);
+
+
+
+#endif // TASK3_H_INCLUDED
