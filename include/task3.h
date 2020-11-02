0dbde908742c123755de214ba49c81594d40d8c7
valvarl <valvarl@ya.ru>
2020-11-02 19:52:52 +0300

    task3.h, main3.h

--- include/task3.h
+++ include/task3.h
@@ -5,4 +5,11 @@
 #ifndef TASK1_TASK3_H
 #define TASK1_TASK3_H
 
+template<typename T, size_t n>
+void map(T arr[], T (*change)(T)) {
+    for (int i = 0; i < n; i++) {
+        arr[i] = change(arr[i]);
+    }
+}
+
 #endif //TASK1_TASK3_H
