73594463b99e04dfd26ff6aa6c48752f643e6611
ArinaMonicheva <63364781+ArinaMonicheva@users.noreply.github.com>
2020-11-03 15:01:52 +0300

    task 3 done

---
+++ include/task3.h
@@ -0,0 +1,16 @@
+#ifndef TASK3_H
+#define TASK3_H
+
+template<typename T, unsigned int arrSize> void map( T (&initialArr)[arrSize], T(*change)(T) )
+{
+	for (int i = 0; i < arrSize; i++){
+		initialArr[i] = change(initialArr[i]);
+	}
+}
+
+#endif
+
+template<typename E> E square(E x)
+{
+	return x*x;
+}
