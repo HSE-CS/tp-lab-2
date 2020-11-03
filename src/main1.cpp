deee581643955d4293b20b6b79eaef98ccb10d27
Ilya Urtyokov <urtilya@gmail.com>
2020-11-03 13:08:31 +0300

    fixed

--- src/main1.cpp
+++ src/main1.cpp
@@ -8,11 +8,11 @@
 using namespace std;
 
 int main(){
-    const size_t size = 5;
-    float array[size]{ 2.0, 213.4, 2.0, -1.22, -1.23};
-    msort(array,size);
-    for (double number : array)
-        cout << number << '\t';
+    const size_t n = 5;
+    int arr[5]={-5,20,15,-50,60}
+    msort(arr,n);
+    for (size_t i=0; i<n;++i)
+        cout << arr[i] << '\t';
     cout<<endl;
     return 0;
 }
