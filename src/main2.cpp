250b34d9c8e7b8317f538aa76163c4a0ea10a119
jswark <swark.a.j@gmail.com>
2020-11-03 18:01:33 +0300

    task2

---
+++ src/main2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "task2.h"
+
+int main() {
+    const size_t n = 5;
+    int* arr;
+    arr = createArr <int, n> (generator);
+    for (int i = 0; i < n; ++i)
+        std::cout << arr[i] << " ";
+
+}
