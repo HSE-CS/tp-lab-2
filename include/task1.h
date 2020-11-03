d8748bff182c81ada733da4a1200b94866056e1f
Ilya Urtyokov <urtilya@gmail.com>
2020-11-03 13:50:34 +0300

    fixed3

--- include/task1.h
+++ include/task1.h
@@ -7,12 +7,22 @@
 
 using namespace std;
 
+template<typename T> bool comp(T a, T b)
+{
+    return a < b;
+}
+
+template<>bool comp<char *>(char *a, char *b)
+{
+    return strlen(a) < strlen(b);
+}
+
 template<typename T>
 void merge(T *arr,size_t n, size_t l, size_t r) {
   T *mas=new T[n];
   size_t mid=(l+r)/2,start=l,fin=mid+1; 
   for(size_t i=l; i<=r; i++) {
-    if ((start<=mid) && ((fin>r) || (arr[start]<arr[fin])))
+    if ((start<=mid) && ((fin>r) || (comp(arr[start],arr[fin]))))
     {
       mas[i]=arr[start];
       start++;
@@ -24,30 +34,9 @@ void merge(T *arr,size_t n, size_t l, size_t r) {
     }
   }
   for (size_t i=l; i<=r; i++) arr[i]=mas[i];
-  delete[]mas;
 
 }
 
-template<>
-void merge(char **arr,size_t n, size_t l, size_t r) {
-  char **mas=new char*[n];
-  size_t mid=(l+r)/2,start=l,fin=mid+1; 
-  for(size_t i=l; i<=r; i++) {
-    if ((start<=mid) && ((fin>r) || (strlen(arr[start])<strlen(arr[fin]))))
-    {
-      mas[i]=arr[start];
-      start++;
-    }
-    else
-    {
-      mas[i]=arr[fin];
-      fin++;
-    }
-  }
-  for (size_t i=l; i<=r; i++) arr[i]=mas[i];
-  delete[]mas;
-
-}
 
 template<typename T>
 void msort(T arr, size_t n, size_t l = 0, size_t r = -1) {
@@ -63,16 +52,5 @@ void msort(T arr, size_t n, size_t l = 0, size_t r = -1) {
 }
 
 
-template<>
-void msort(char** arr, size_t n, size_t l, size_t r) {
-    if (r==-1){
-      r=n-1;
-    }
-    if (l<r){
-      int mid=(l+r)/2;
-      msort(arr,mid+1,0,mid);
-      msort(arr,n-mid-1,mid+1,r);
-      merge(arr,n,l,r);
-    }
-}
+
 #endif 
