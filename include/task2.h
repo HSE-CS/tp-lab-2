e418f4bd3d8564f99376d164cb563958a9b36a17
Michail <mdmurzinov@edu.hse.ru>
2020-11-03 19:54:19 +0300

    Lab2 Murzinov Michail b18

---
+++ include/task2.h
@@ -0,0 +1,16 @@
+#ifndef TASK2_H
+#define TASK2_H
+#include <iostream>
+#include <string.h>
+
+
+template<typename T, size_t N> T*
+createArr(T(*gen)())
+{
+	T* array = new T[N];
+	for (int i = 0; i < N; i++)
+		array[i] = gen();
+	return array;
+}
+
+#endif
