c54607e7a77209c5bfa289fc3d963e4d544e52a5
Ilya Urtyokov <urtilya@gmail.com>
2020-11-03 13:24:21 +0300

    fixed2

--- include/task2.h
+++ include/task2.h
@@ -9,7 +9,7 @@ using namespace std;
 template<typename T, size_t n>
 T *createArr(T (*gen)()) {
     T *arr = new T[n];
-    for (size_t i = 0; i < n; ++i)
+    for (size_t i = 0; i < n; i++)
         arr[i] = gen();
     return arr;
 }
--- src/main1.cpp
+++ src/main1.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
 int main(){
     const size_t n = 5;
-    int arr[5]={-5,20,15,-50,60}
+    int arr[5]={-5,20,15,-50,60};
     msort(arr,n);
     for (size_t i=0; i<n;++i)
         cout << arr[i] << '\t';
