ed433a2b10879395e61a1cbada687e59e74d275e
Your NameVadukk <Vadik-Tumakov.com@yandex.ru>
2020-11-03 20:41:57 +0300

    Tumakov task1 fix

--- include/task1.h
+++ include/task1.h
@@ -1,61 +1,139 @@
-#ifndef TASK1_TASK1_H
-#define TASK1_TASK1_H
-
+#include <iostream>
 #include <cstring>
 
 template <typename T>
-bool comp(T a, T b) {
-	return (a <= b);
+void merge(T* array, size_t left, size_t middle, size_t right)
+{
+	size_t len_left = middle - left + 1;
+	size_t len_right = right - middle;
+
+	T* Left = new T[len_left];
+	T* Right = new T[len_right];
+
+	for (size_t i = 0; i < len_left; i++)
+		Left[i] = array[left + i];
+
+	for (size_t j = 0; j < len_right; j++)
+		Right[j] = array[middle + 1 + j];
+
+	size_t ind_Left = 0, ind_Right = 0;
+
+	size_t ind_Array = left;
+
+	while (ind_Left < len_left && ind_Right < len_right)
+	{
+		if (Left[ind_Left] <= Right[ind_Right])
+		{
+			array[ind_Array] = Left[ind_Left];
+			ind_Left++;
+		}
+		else
+		{
+			array[ind_Array] = Right[ind_Right];
+			ind_Right++;
+		}
+		ind_Array++;
+	}
+
+	while (ind_Left < len_left)
+	{
+		array[ind_Array] = Left[ind_Left];
+		ind_Left++;
+		ind_Array++;
+	}
+
+	while (ind_Right < len_right)
+	{
+		array[ind_Array] = Right[ind_Right];
+		ind_Right++;
+		ind_Array++;
+	}
 }
+
+
+template<>
+void merge(char** array, size_t left, size_t middle, size_t right)
+{
+	size_t len_left = middle - left + 1;
+	size_t len_right = right - middle;
+
+	char** Left = new char* [len_left];
+	char** Right = new char* [len_right];
+
+	for (size_t i = 0; i < len_left; i++)
+		Left[i] = array[left + i];
+
+	for (size_t j = 0; j < len_right; j++)
+		Right[j] = array[middle + 1 + j];
+
+	size_t ind_Left = 0, ind_Right = 0;
+
+	size_t ind_Array = left;
+
+	while (ind_Left < len_left && ind_Right < len_right)
+	{
+		if (strlen(Left[ind_Left]) <= strlen(Right[ind_Right]))
+		{
+			array[ind_Array] = Left[ind_Left];
+			ind_Left++;
+		}
+		else
+		{
+			array[ind_Array] = Right[ind_Right];
+			ind_Right++;
+		}
+		ind_Array++;
+	}
+
+	while (ind_Left < len_left)
+	{
+		array[ind_Array] = Left[ind_Left];
+		ind_Left++;
+		ind_Array++;
+	}
+
+	while (ind_Right < len_right)
+	{
+		array[ind_Array] = Right[ind_Right];
+		ind_Right++;
+		ind_Array++;
+	}
+}
+
+
+template <typename T>
+void msort(T* array, size_t size, size_t left = 0, size_t right = 0)
+{
+	if (size != 0) {
+		right = size - 1;
+	}
+
+	if (left < right)
+	{
+		size_t middle = left + (right - left) / 2;
+
+		msort(array, 0, left, middle);
+		msort(array, 0, middle + 1, right);
+
+		merge(array, left, middle, right);
+	}
+}
+
 
 template <>
-bool comp(char* a, char* b) {
-	return (strlen(a) < strlen(b));
-}
-
-template<typename T>
-void merge(T* a, int first, int last) {
-
-	int left_part, right_part, middle, num = 0, j = 0;
-	T* sorted_part = new T[last + 1];
-	left_part = first;
-	right_part = (first + last) / 2;
-	middle = right_part - 1;
-
-	for (int i = first; i < last; i++) {
-		if ((left_part <= middle) && ((right_part >= last) || comp(a[left_part], a[right_part]))) {
-			sorted_part[num++] = a[left_part];
-			left_part++;
-		}
-		else {
-			sorted_part[num++] = a[right_part];
-			right_part++;
-		}
+void msort(char** array, size_t size, size_t left, size_t right)
+{
+	if (size != 0) {
+		right = size - 1;
 	}
 
-	for (int i = first; i < last; i++) {
-		a[i] = sorted_part[j];
-		j++;
-	}
+	if (left < right)
+	{
+		size_t middle = left + (right - left) / 2;
 
-	delete[] sorted_part;
-}
+		msort(array, 0, left, middle);
+		msort(array, 0, middle + 1, right);
 
-template <typename T>
-void merge_sort(T* a, int first, int last) {
-	if (first < last - 1) {
-		int middle = first + last;
-		middle = (middle) / 2;
-
-		merge_sort(a, first, middle);
-		merge_sort(a, middle, last);
-		merge(a, first, last);
+		merge(array, left, middle, right);
 	}
 }
-
-template <typename T>
-void msort(T* a, size_t n) {
-	merge_sort(a, 0, n);
-}
-
-#endif
--- src/main1.cpp
+++ src/main1.cpp
@@ -1,21 +1,16 @@
-#include <iostream>
 #include "task1.h"
 
 int main() {
-	const size_t n = 6;
 
-	double arr[n]{ 4.0, 3.0, 2.4, 2.5, 2.1, -1 };
-	msort(arr, n);
-	for (double i : arr)
-		std::cout << i << ' ';
+	const size_t size = 3;
+	char* val[size]{ (char*)"ab", (char*)"abc", (char*)"abcd" };
 
-	std::cout << std::endl;
+	msort(val, size);
 
-	char* arr1[n]{ (char*)"ffff", (char*)"fff2", (char*)"f", (char*)"ff", (char*)"vvv", (char*)"fffrr" };
-	msort(arr1, n);
-	for (auto& i : arr1)
-		std::cout << i << ' ';
-
+	for (int i = 0; i < size; i++)
+	{
+		std::cout << val[i] << " ";
+	}
 
 	return 0;
 }
