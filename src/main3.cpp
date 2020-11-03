e48083b15a5fdea70efce7f291a3a766b2562292
ArinaMonicheva <63364781+ArinaMonicheva@users.noreply.github.com>
2020-11-03 15:33:56 +0300

    func change() now here, added example with char
    Func name is square. Specialisation for char type is excessive due to ascii representation in int type and each symbol is printed separately

--- src/main3.cpp
+++ src/main3.cpp
@@ -3,21 +3,38 @@
 
 using namespace std;
 
+template<typename E> E square(E x) //specialisation for char type is excessive due to ascii representation in int type and each symbol is printed separately
+{
+	return x*x;
+}
+
 int main()
 {
 	int randArr[] = { 7, 12, -2, 3, 1, 0, -5 };
 	float randArrD[] = { 7.3, 12.1, -2.3, 3.7, 1, 0, -5.4 };
+	char stringArr[] = { "hello" };
+
 	map(randArr, square);
 	map(randArrD, square);
+	map(stringArr, square);
+
 	for (auto item : randArr)
 	{
 		cout << item << ' ';
 	}
 	cout << endl;
+
 	for (auto item : randArrD)
 	{
 		cout << item << ' ';
 	}
 	cout << endl;
+
+	for (auto item : stringArr)
+	{
+		cout << item << ' ';
+	}
+	cout << endl;
+
 	return 0;
 }
