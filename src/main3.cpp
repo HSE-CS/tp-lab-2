68e9c1e3029611bba74f507820e193047d2e3fa6
jswark <swark.a.j@gmail.com>
2020-11-03 18:16:47 +0300

    task3

---
+++ src/main3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "task3.h"
+
+int main() {
+    const size_t n = 5;
+    int arr[n]{1, 2, 3, 4, 5};
+    map <int, n> (arr, change);
+    for (int i : arr)
+        std::cout << i << " ";
+
+    return 0;
+}
