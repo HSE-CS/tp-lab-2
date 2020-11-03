497f7a9f3214fe85ee787db683d923f3d4554cc6
Polina Ozhiganova <44429468+DoktaPola@users.noreply.github.com>
2020-11-03 18:48:34 +0300

    fix

--- include/task3.h
+++ include/task3.h
@@ -5,20 +5,6 @@
 #include <iostream>
 #include <cstring>
 
-template<class T>
-T change(T val) {
-    return val + 1;
-}
-
-template<>
-char *change(char *val) {
-    size_t n = strlen(val);
-    for (size_t i = 0; i < n; i++)
-        val[i] = val[i] + 1;
-    return val;
-}
-
-
 template<typename T, size_t size>
 void map(T *arr, T (*change)(T)) {
     for (auto i = 0; i < size; ++i) {
