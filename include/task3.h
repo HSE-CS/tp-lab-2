30fb4d12cb8f55464d724e550f95819a2c704c96
Stolbov Yaroslav <stojar2001@gmail.com>
2020-11-04 15:24:48 +0300

    Third task

--- include/task3.h
+++ include/task3.h
@@ -4,5 +4,14 @@
 
 #ifndef TASK1_TASK3_H
 #define TASK1_TASK3_H
-
+#include <iostream>
+#include <typeinfo>
+template <typename T, size_t size>
+void map(T *arr, T(*change)(T))
+{
+    for (size_t i = 0; i < size; i++)
+    {
+        arr[i] = change(arr[i]);
+    }
+}
 #endif //TASK1_TASK3_H
--- src/main3.cpp
+++ src/main3.cpp
@@ -5,7 +5,20 @@
 #include "task3.h"
 #include <iostream>
 
-int main(){
+template <typename T> T change(T val)
+{
+    return val + 10;
+}
 
+using namespace std;
+int main(){
+    const size_t n = 5;
+    int arr[n]{1, 2, 3, 4, 5};
+    map<int, n>(arr, change);
+
+    int expected[n]{2, 3, 4, 5, 6};
+
+    for(size_t i = 0; i < n; i++)
+        cout<<expected[i]<< " " << arr[i]<<"\n";
     return 0;
 }
