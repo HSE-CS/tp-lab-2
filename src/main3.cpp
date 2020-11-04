7e9adfea2734b79c2fa8a4612af5ef23ee42a09a
natasha <igum.natasha@gmail.com>
2020-11-04 13:49:53 +0300

    Add task3

---
+++ src/main3.cpp
@@ -0,0 +1,14 @@
+#include "task3.h"
+
+int main()
+{
+	const int n = 10;
+	int arr[n] = { 1,2,3,4,5,6,7,8,9,10 };
+	map<int, n>(arr, change);
+	for (size_t i = 0; i < n; i++)
+	{
+		std::cout << arr[i] << " ";
+	}
+	
+
+}
