30ce6dd2cb1eed7669a27baae7452fafd52a120d
bekina-svetlana <bekina.swetlana@yandex.ru>
2020-11-05 00:16:10 +0300

    task 2

---
+++ src/main2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "task2.h"
+#include <string.h>
+
+using namespace std;
+
+template<class T>
+T gen()
+{
+	return 0;
+}
+
+int main() {
+	const size_t n = 5;
+	int* arr;
+	arr = createArr<int, n>(gen);
+	for (size_t i = 0; i < n; i++) {
+		cout << arr[i] << ' ';
+	}
+	delete[] arr;
+}
