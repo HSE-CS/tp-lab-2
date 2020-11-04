3b8ee1d5dc3d3e4c0ef1bdc16e9de260503625c1
bekina-svetlana <bekina.swetlana@yandex.ru>
2020-11-05 00:14:51 +0300

    task 1

---
+++ src/main1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "task1.h"
+#include <string.h>
+
+using namespace std;
+
+int main() {
+	const size_t n = 10;
+	int val[n]{ 100, 8, -5, -2, 0, 9, 0, 100, -7, -5 };
+	msort(val, n);
+	for (size_t i = 0; i < n; i++) {
+		cout << val[i] << ' ';
+	}
+}
