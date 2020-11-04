3d1d76a5bfddd303ee0d66284c8cb5f7289a0cd2
nikit <nikita201721@gmail.com>
2020-11-04 15:48:57 +0300

    Second commit

--- include/task3.h
+++ include/task3.h
@@ -5,21 +5,6 @@
 #include <cstring>
 using namespace std;
 
-template<class T>
-T change(T val)
-{
-    return val + 1;
-}
-
-template<>
-char* change(char* val)
-{
-    size_t n = strlen(val);
-    for(size_t i = 0; i < n; i++)
-        val[i] = val[i] + 1;
-    return val;
-}
-
 template<typename T, size_t size>
 void map(T arr[], T(*change)(T)) {
     for (int i = 0; i < size; i++)
