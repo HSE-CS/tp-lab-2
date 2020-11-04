5493845499ea54bfb21ec272f69d85dbe3ec84f3
Stolbov Yaroslav <stojar2001@gmail.com>
2020-11-04 15:05:24 +0300

    second task 2

--- include/task2.h
+++ include/task2.h
@@ -4,5 +4,38 @@
 
 #ifndef TASK1_TASK2_H
 #define TASK1_TASK2_H
+#include <iostream>
+#include <typeinfo>
+#include <cstring>
 
+using namespace std;
+
+template<typename T, size_t size>
+
+T* createArr(T(*gen)()) {
+    T* array = new T[size];
+    for (size_t i = 0; i < size; i++)
+        array[i] = gen();
+    return array;
+}
+template<class T>
+T gen()
+{
+    static int k = 48;
+    return k++;
+}
+
+template<>
+char* gen()
+{
+    static unsigned k = 0;
+    k++;
+    char* s = new char[k + 1];
+
+    for (int i = 0; i < k; i++)
+        s[i] = '0' + i;
+    s[k] = '\0';
+
+    return s;
+}
 #endif //TASK1_TASK2_H
--- src/main2.cpp
+++ src/main2.cpp
@@ -5,7 +5,47 @@
 #include "task2.h"
 #include <iostream>
 
+
+
 int main(){
 
+        const size_t n = 5;
+        int* arr;
+        arr = createArr<int, n>(gen);
+
+        int* expected = new int[n] {48, 49, 50, 51, 52};
+
+        for (int i = 0; i < n; i++)
+            cout << expected[i] << " " << arr[i] << endl;
+        delete[] arr;
+        delete[] expected;
+
+        char** arr3;
+        arr3 = createArr<char*, n>(gen);
+
+        char** expected3 = new char* [n];
+        for (int i = 0; i < n; i++)
+        {
+            expected3[i] = new char[i + 2];
+            for (int j = 0; j < i + 1; j++)
+                expected3[i][j] = '0' + j;
+            expected3[i][i + 1] = '\0';
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            cout << strlen(expected3[i]) << " " << strlen(arr3[i]) << endl;
+            if (strcmp(expected3[i], arr3[i]))
+                for (int j = 0; j <= strlen(expected3[i]); j++)
+                    cout << expected3[j] << " " << arr3[j] << endl;
+        }
+        for (int i = 0; i < n; i++)
+        {
+            delete[] arr3[i];
+            delete[] expected3[i];
+        }
+
+        delete[] arr3;
+        delete[] expected3;
     return 0;
 }
