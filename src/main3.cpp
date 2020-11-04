a8fefd9020a7195cbc8da6d57b5ec4845b0911bd
bekina-svetlana <bekina.swetlana@yandex.ru>
2020-11-05 00:17:29 +0300

    task 3

---
+++ src/main3.cpp
@@ -0,0 +1,20 @@
+#include "task3.h"
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+template<class T>
+T change(T val)
+{
+	return val + 1;
+}
+
+int main() {
+	const size_t n = 10;
+	int arr[n]{ 100, 8, -5, -2, 0, 9, 0, 100, -7, -5 };
+	map<int, n>(arr, change);
+	for (size_t i = 0; i < n; i++) {
+		cout << arr[i] << ' ';
+	}
+}
