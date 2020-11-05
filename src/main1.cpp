b3672df721c85593b7d4523573d62d728a50df1b
ArinaMonicheva <63364781+ArinaMonicheva@users.noreply.github.com>
2020-11-06 00:11:24 +0300

    task 1 done

---
+++ src/main1.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string.h>
+#include "task1.h"
+#define SIZE 12
+
+using namespace std;
+
+int main()
+{
+	int *intsArr = new int[SIZE]{ 4, -5, 2, 0, 23, 68, -11, 4, 4, 3, 29, 1 };
+	double *dArr = new double[SIZE]{ 4.5, 2.9, -1.1, 0, 1, 23.6, 12, 70.03, 70.1, -3.4, 11, 11.2 };
+	char *charsArr = new char[SIZE+1]{ "JU3981zU#u&4" };
+	char *strings[SIZE] = { "taki", "kew", "iuwi8", "89tb", "654s", "_&^^5", "jr5", "jckxy", "7ftc", "j6", "gtuyxzdufyiu", "y7t865drt" };
+	char **strArr = new char*[SIZE];
+
+	for (int i = 0; i < SIZE; i++) {
+		strArr[i] = new char[strlen(strings[i] + 1)];
+		for (int j = 0; j < strlen(strings[i]); j++) {
+			strArr[i][j] = strings[i][j];
+		}
+		strArr[i][strlen(strings[i])] = '\0';
+	}
+
+	msort(intsArr, 0, SIZE - 1);
+	msort(dArr, 0, SIZE - 1);
+	msort(charsArr, 0, SIZE - 1);
+	msort(strArr, 0, SIZE - 1);
+
+	for (int i = 0; i < SIZE; i++) {
+		cout << intsArr[i] << ' ';
+	}
+	cout << endl;
+
+	for (int i = 0; i < SIZE; i++) {
+		cout << dArr[i] << ' ';
+	}
+	cout << endl;
+
+	for (int i = 0; i < SIZE; i++) {
+		cout << charsArr[i] << ' ';
+	}
+	cout << endl;
+
+	for (int i = 0; i < SIZE; i++) {
+		cout << strArr[i] << endl;
+	}
+
+	delete[]intsArr;
+	delete[]dArr;
+	delete[]charsArr;
+	delete[]strArr;
+
+	return 0;
+}
