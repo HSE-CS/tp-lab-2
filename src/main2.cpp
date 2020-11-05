8ba44ccd7eb91e0821fc39ce38b49a6c7a510f68
Nikita Kasyanov <kasyanikita@gmail.com>
2020-11-05 22:51:47 +0300

    Second attempt

--- src/main2.cpp
+++ src/main2.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 #include "../include/task2.h"
 
+int gen(){
+    static int a = 10;
+    return a++;
+}
+
 int main() {
-    auto a = createArr<char, 10>(gen);
+    auto a = createArr<int, 10>(gen);
     for (size_t i = 0; i < 10; ++i) {
         std::cout << a[i] << ' ';
     }
--- src/main3.cpp
+++ src/main3.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include "../include/task3.h"
 
+int change(int v) {
+    return v + 1;
+}
+
 int main() {
     const size_t n = 5;
-    char arr[n]{'1', '2', '3', '4', '5'};
-    map<char, n>(arr, change);
+    int arr[n]{1, 2, 3, 4, 5};
+    map<int, n>(arr, change);
     for (size_t i = 0; i < n; i++) {
         std::cout << arr[i] << ' ';
     }
