9242b87bd5287578e41305c0ac5775e5677fa54f
SmirnovGrigory <63301523+SmirnovGrigory@users.noreply.github.com>
2020-11-05 13:18:03 +0300

    Update main2.cpp

--- src/main2.cpp
+++ src/main2.cpp
@@ -1,29 +1,6 @@
 #include "task2.h"
 
-/*template<class T>
-T gen()
-{
-	static int t = 48;
-	return t++;
-}
-template<>
-char* gen()
-{
-	static unsigned t = 0;
-	t++;
-	char* s = new char[t + 1];
-
-	for (size_t i = 0; i < t; i++)
-		s[i] = '0' + i;
-	s[t] = '\0';
-
-	return s;
-}
-
 int main()
 {
-	char** my_arr = createArr<char*, 10>(gen);
-	for (int i=0;i<10;i++)
-		cout << my_arr[i] << " ";
-
-}*/
+	return 0;
+}
