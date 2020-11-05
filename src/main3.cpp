6ab316cfff4659740206474d042acff6622dd809
SmirnovGrigory <63301523+SmirnovGrigory@users.noreply.github.com>
2020-11-05 13:18:44 +0300

    Update main3.cpp

--- src/main3.cpp
+++ src/main3.cpp
@@ -1,24 +1,6 @@
-/*#include "task3.h"
-
-template<class T>
-T change(T val)
-{
-	return val + 1;
-}
-
-template<>
-char* change(char* val)
-{
-	size_t n = strlen(val);
-	for (size_t i = 0; i < n; i++)
-		val[i] = val[i] + 1;
-	return val;
-}
+#include "task3.h"
 
 int main()
 {
-	char my_arr[4] = { 'a','b','c','d' };
-	map(my_arr, change);
-	for (auto item : my_arr)
-		cout << item<<" ";
-}*/
+	return 0;
+}
