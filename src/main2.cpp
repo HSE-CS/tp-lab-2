899130e23779dd2e155e97922a48c0917bf7b68a
m_khorosh <marina.khoroshilova@gmail.com>
2020-11-05 21:54:41 +0300

    add task2

---
+++ src/main2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "task2.h"
+#include <string>
+#include <cstring>
+
+int main(){
+    const size_t n = 5;
+    char* arr;
+    arr = createArr<char, n>(gen);
+
+    char *expected = new char[n]{48, 49, 50, 51, 52};
+
+    for(size_t i = 0; i < n; i++)
+        std::cout<<expected[i]<<' '<<arr[i]<<std::endl;
+    delete[] arr;
+    delete[] expected;
+}
