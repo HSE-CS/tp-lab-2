49b35b554a83a850eaa18653b0201526c4b835b7
AlexeusC4ever <62253233+AlexeusC4ever@users.noreply.github.com>
2020-11-06 18:10:42 +0300

    Update main1.cpp

--- src/main1.cpp
+++ src/main1.cpp
@@ -3,9 +3,28 @@
 #include "task3.h"
 #include <iostream>
 #include <typeinfo>
+#include <cstring>
 
+int generator() {
+	int num = 0;
+	std::cin >> num;
+	return num;
+}
+
+int change(int a) {
+	return a * 2 + 34;
+}
 
 int main() {
+	/*int* mas = new int[5];
+	for (int i = 0; i < 5; ++i) {
+		std::cin >> mas[i];
+	}
+	map<int, 5>(mas, &change);
+
+	for (int i = 0; i < 5; ++i) {
+		std::cout << mas[i] << " ";
+	}*/
 	int a[3] = { 5,2,7 };
 
 	char buf[100];
@@ -16,8 +35,5 @@ int main() {
 		strcpy(mas2[i], buf);
 	}
 	msort(mas2, 4);
-	for (int i = 0; i < 4; ++i) {
-		std::cout << mas2[i] << "\n";
-	}
 
 }
