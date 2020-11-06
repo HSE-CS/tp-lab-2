7e6d2983627c4fbe5cd43cf3e9a7edff5c87e964
Your Name <olga.frolova.2001@bk.ru>
2020-11-06 17:37:01 +0300

    fix

--- include/task1.h
+++ include/task1.h
@@ -1,15 +1,15 @@
 #include <iostream>
 #include <string.h>
 
-
-template<typename T>
-bool compare(T a, T b) {
-    return a < b;
-}
-
-template <> bool compare <char*>(char* a, char* b) {
-    return strlen(a) < strlen(b);
-}
+//
+//template<typename T>
+//bool compare(T a, T b) {
+//    return a < b;
+//}
+//
+//template <> bool compare <char*>(char* a, char* b) {
+//    return strlen(a) < strlen(b);
+//}
 
 template<typename T>
 void merge(T arr[],size_t begin, size_t end)
@@ -19,7 +19,7 @@ void merge(T arr[],size_t begin, size_t end)
 
     while (i <= mid && j<= end) {
 
-        if (compare(arr[i],arr[j])) {
+        if (arr[i]<arr[j]) {
             d[t] = arr[i]; 
             i++;
         }
@@ -45,8 +45,45 @@ void merge(T arr[],size_t begin, size_t end)
 
     for (i = 0; i < t; i++)
         arr[begin + i] = d[i];
+    delete[] d;
 }
 
+template<> void merge<char*>(char* arr[], size_t begin, size_t end)
+
+{
+    size_t i = begin, t = 0, mid = begin + (end - begin) / 2, j = mid + 1;
+    char** d = new char*[end + 1];
+
+    while (i <= mid && j <= end) {
+
+        if (strlen(arr[i])<strlen(arr[j])) {
+            d[t] = arr[i];
+            i++;
+        }
+        else {
+            d[t] = arr[j];
+            j++;
+        }
+        t++;
+    }
+
+    while (i <= mid) {
+        d[t] = arr[i];
+        i++;
+        t++;
+
+    }
+
+    while (j <= end) {
+        d[t] = arr[j];
+        j++;
+        t++;
+    }
+
+    for (i = 0; i < t; i++)
+        arr[begin + i] = d[i];
+    delete[] d;
+}
 
 template<typename T>
 void msort(T* arr, size_t N, size_t left, size_t right)
