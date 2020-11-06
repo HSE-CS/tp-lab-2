62fc37f4479cb20fd20e337a5ef4ab9cbf784c37
Vlad <zagoskin.3@mail.ru>
2020-11-06 11:13:34 +0300

    1

--- include/task1.h
+++ include/task1.h
@@ -55,58 +55,50 @@ void msort(T * mas, int len)
         n *= 2;
     }
 }
-void msort(char * a[], int num)
-{
+template<>
+void msort<char*>(char** arr, unsigned int n) {
 
-    int rght, rend;
-    int i, j, m;
-    char * b[num];
-    for(int i = 0; i < num; i++)
-    {
-    	b[i] = (char*)calloc(100,sizeof(char));
-    }
- 
-    for (int k = 1; k < num; k *= 2)
-    {
-        for (int left = 0; left + k < num; left += k * 2)
-        {
-            rght = left + k;
-            rend = rght + k;
-            if (rend > num) rend = num;
-            m = left; i = left; j = rght;
-            while (i < rght && j < rend)
-            {
-                if (strlen(a[i]) < strlen(a[j]))
-                {
- 
-                   strcpy(b[m],a[i]); i++;
-                }
-                else
-                {
-                    strcpy(b[m],a[j]); j++;
-                }
-                m++;
-            }
-            while (i < rght)
-            {
-                strcpy(b[m],a[i]);
-                i++; m++;
-            }
-            while (j < rend)
-            {
-                strcpy(b[m],a[j]);
-                j++; m++;
-            }
-            for (m = left; m < rend; m++)
-            {
-            	a[m] = (char*)realloc(NULL,strlen(b[m]));
-                strcpy(a[m],b[m]);
-            }
-        }
-    }
-    for(int i = 0; i < num; i++)
-    	free(b[i]);
+	
+	char** _res = new char* [n];
+	for (size_t k = 0; k < n; ++k) {
+		_res[k] = new char[n];
+	}
 
+
+	for (size_t i = 1; i < n; i *= 2) {
+		for (size_t j = 0; j < n - i; j += 2 * i) {
+
+			unsigned int _iter1{ 0 };
+			unsigned int _iter2{ 0 };
+			unsigned int middle = j + i;
+			unsigned int right = min(j + 2 * i, n);
+			unsigned int left = j;
+			
+			while ((_iter1 + left < middle) && (_iter2 + middle < right)) {
+				if (strlen(arr[left + _iter1]) < strlen(arr[middle + _iter2])) {
+					copy(_res[_iter1 + _iter2], arr[left + _iter1]);
+					_iter1++;
+				}
+				else {
+					copy(_res[_iter1 + _iter2], arr[middle + _iter2]);
+					_iter2++;
+				}
+			}
+
+			while (left + _iter1 < middle) {
+				copy(_res[_iter1 + _iter2], arr[left + _iter1]);
+				_iter1++;
+			}
+			while (middle + _iter2 < right) {
+				copy(_res[_iter1 + _iter2], arr[middle + _iter2]);
+				_iter2++;
+			}
+
+			for (size_t w = 0; w < _iter1 + _iter2; ++w) {
+				copy(arr[left + w], _res[w]);
+			}
+		}
+	}
 }
 
 
