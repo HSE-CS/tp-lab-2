24412516741f3eb7df8eaed9d689badbc10cbf7e
AlexeusC4ever <62253233+AlexeusC4ever@users.noreply.github.com>
2020-11-06 16:07:47 +0300

    Update task3.h

--- include/task3.h
+++ include/task3.h
@@ -5,10 +5,9 @@
 
 template <typename T, size_t N>
 
-T* map(T* mas, T(*change)()) {
+T* map(T* mas, T(*change)(T a)) {
 	for (int i = 0; i < N; ++i) {
-		mas[i] = change();
+		mas[i] = change(mas[i]);
 	}
 	return mas;
 }
-
