bb0f4588cda228f057aab00d86cb982b8d8ace82
Elizaveta Drobot <lizunchik7@bk.ru>
2020-11-07 20:24:00 +0300

    task2

---
+++ src/main2.cpp
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "task2.h"
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+
+
+int main()
+{
+    const size_t size = 5;
+    double* array;
+    array = createArr<double, size>(gen);
+    for (size_t i = 0; i < size; i++) {
+        std::cout << array[i] << " ";
+    }
+    return 0;
+}
