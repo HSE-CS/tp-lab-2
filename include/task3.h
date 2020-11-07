741aa094d3c67d43ff7799f2c2ea21575ea61e46
GlebNeshchetkin <glebneshchetkin@gmail.com>
2020-11-07 16:30:10 +0300

    Test

---
+++ include/task3.h
@@ -0,0 +1,15 @@
+#ifndef TASK1_H
+#define TASK1_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <cstdlib>
+#include <iostream>
+#include <cstring>
+using namespace std;
+template <typename T>
+void map(T* a, int n)
+{
+
+}
+#endif
