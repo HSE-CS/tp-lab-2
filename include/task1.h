6f85a07d1d92b04cbb1bafe421f91d3ff9cf1dce
Andrew <55204554+Void-fun@users.noreply.github.com>
2020-11-07 17:15:36 +0300

    I'm a thief

--- include/task1.h
+++ include/task1.h
@@ -6,6 +6,19 @@
 #include <string.h>
 
 template <typename T>
+bool raznitsa(T a, T b)
+{
+    return (a < b);
+}
+
+template <> bool raznitsa <char*>( char* a,  char* b)
+{
+    return strlen(a) < strlen(b);
+}
+
+
+
+template <typename T>
 void copy(T* from, T* to, size_t left, size_t right) {
     for (size_t i = left; i < right; i++) {
         to[i - left] = from[i];
@@ -13,53 +26,9 @@ void copy(T* from, T* to, size_t left, size_t right) {
 }
 
 
-void charmsort(char** arr, size_t n) {
-    if (n > 1) {
-        size_t left_border{ n / 2 }, right_border{ n - left_border };
-        charmsort(&arr[0], left_border);
-        charmsort(&arr[left_border], right_border);
-        size_t lidx = 0, ridx = left_border, idx = 0;
-        char** tmp_arr{ 0 };
-        tmp_arr = new char* [n];
-        while (lidx < left_border || ridx < n)
-        {
-            if (strlen(arr[lidx]) < strlen(arr[ridx]))
-            {
-                tmp_arr[idx++] = std::move(arr[lidx]);
-                lidx++;
-            }
-            else
-            {
-                tmp_arr[idx++] = std::move(arr[ridx]);
-                ridx++;
-            }
-
-            if (lidx == left_border)
-            {
-                copy(arr, &tmp_arr[idx], ridx, n);
-                break;
-            }
-            if (ridx == n)
-            {
-                copy(arr, &tmp_arr[idx], lidx, left_border);
-                break;
-            }
-        }
-
-        copy(tmp_arr, arr, 0, n);
-    }
-}
-
-
 template <typename T>
 void msort(T arr[], size_t n) noexcept {
-    char* type = (char*)typeid(*arr).name();
-	if (type[0] == 'c') {
-		// TODO: sort
-        charmsort((char**)arr, n);
-	}
-	else {
-        if (n > 1) {
+         if (n > 1) {
             size_t left_border{ n / 2 }, right_border{ n - left_border };
             msort(&arr[0], left_border);
             msort(&arr[left_border], right_border);
@@ -69,7 +38,7 @@ void msort(T arr[], size_t n) noexcept {
             
             while (lidx < left_border || ridx < n)
             {
-                if (arr[lidx] < arr[ridx])
+                if (raznitsa(arr[lidx], arr[ridx]))
                 {
                     tmp_arr[idx++] = arr[lidx];
                     lidx++;
@@ -97,8 +66,7 @@ void msort(T arr[], size_t n) noexcept {
 
            // std::copy(std::make_move_iterator(tmp_arr), std::make_move_iterator(&tmp_arr[n]), arr);
             copy(tmp_arr, arr, 0, n);
-        }
-	}
+         }
 }
 
 
