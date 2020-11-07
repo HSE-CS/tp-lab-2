3a7b2b1d19d2fddf15c090c553b18e3c3b2dccd9
HabKaffee <neknaum1@gmail.com>
2020-11-07 19:56:38 +0300

    task 2 done

---
+++ include/task2.h
@@ -0,0 +1,24 @@
+#ifndef TASK2_H
+#define TASK2_H
+
+#include <climits>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+template <typename T, size_t N> T* createArr(T (*gen)()){
+    T* array = new T[N];
+    for (size_t i = 0; i<N;++i){
+        array[i] = gen();
+    }
+    return array;
+}
+
+template <typename T>T generate(){
+    T start = -100;
+    T finish = 1000;
+    T result = start + (double)rand() / RAND_MAX * (finish - start);
+    return result;
+}
+
+#endif
