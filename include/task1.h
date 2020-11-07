86febd56dc57e6b224d3eb892ff4b64a3ee1efb1
danek0100 <71225718+danek0100@users.noreply.github.com>
2020-11-07 23:14:57 +0300

    Fisrt Task

--- include/task1.h
+++ include/task1.h
@@ -1,8 +1,116 @@
 #ifndef TASK1_H
 #define TASK1_H
 #define _CRT_SECURE_NO_WARNINGS
+#include <string>
+#include <iostream>
+#include <cstring>
 
+typedef const char** stroka;
 
+template <typename T> void msort(T* array, const int num_el)
+{
+        int step = 1;
+        T* temp = new T[num_el];
+        while (step < num_el)  
+        {
+            int index = 0;    
+            int left = 0;      
+            int mid = left + step;  
+            int right = left + step * 2;  
+            do
+            {
+                if (mid < num_el)
+                    mid = mid;
+                else mid = num_el; 
+                if (right < num_el)
+                    right = right;
+                else right = num_el;
+                int i1 = left;
+                int i2 = mid;
+                for (; i1 < mid && i2 < right; )
+                {
+                    if (array[i1] < array[i2]) 
+                    {
+                        temp[index++] = array[i1++];
+                    }
+                    else 
+                    {
+                        temp[index++] = array[i2++]; 
+                    }
+                }
+           
+                while (i1 < mid) temp[index++] = array[i1++]; 
+                while (i2 < right) temp[index++] = array[i2++]; 
+                left += step * 2; 
+                mid += step * 2;
+                right += step * 2;
+            } while (left < num_el); 
+            for (int i = 0; i < num_el; i++)
+                array[i] = temp[i];
+            step *= 2;
+        }
+        delete[] temp;
+}
+
+template<>
+void msort <char*>(char** array, const int num_el)
+{
+    int step = 1;
+    char** temp = new char* [num_el];
+    while (step < num_el)
+    {
+        int index = 0;
+        int left = 0;
+        int mid = left + step;
+        int right = left + step * 2;
+        do
+        {
+            if (mid < num_el)
+                mid = mid;
+            else mid = num_el;
+            if (right < num_el)
+                right = right;
+            else right = num_el;
+            int i1 = left;
+            int i2 = mid;
+            for (; i1 < mid && i2 < right; )
+            {
+                if (strlen(array[i1]) < strlen(array[i2]))
+                {
+                    temp[index] = new char[strlen(array[i1])+1];
+                    strcpy(temp[index++], array[i1++]);
+                }
+                else
+                {
+                    temp[index] = new char[strlen(array[i2]) + 1];
+                    strcpy(temp[index++], array[i2++]);
+                }
+            }
+
+            while (i1 < mid)
+            {
+                temp[index] = new char[strlen(array[i1]) + 1];
+                strcpy(temp[index++], array[i1++]);
+            }
+            while (i2 < right)
+            {
+                temp[index] = new char[strlen(array[i2]) + 1];
+                strcpy(temp[index++], array[i2++]);
+            }
+            left += step * 2;
+            mid += step * 2;
+            right += step * 2;
+
+            
+        } while (left < num_el);
+        for (int i = 0; i < num_el; i++)
+            std::swap<char*>(array[i], temp[i]);
+        for (size_t i = 0; i < num_el; i++)
+            delete[] temp[i];
+        step *= 2;
+    }
+    delete[] temp;
+}
 
 #endif
 
--- src/main1.cpp
+++ src/main1.cpp
@@ -1,11 +1,17 @@
 #include "task1.h"
-#define _CRT_SECURE_NO_WARNINGS
 
-
-
+using namespace std;
 
 int main()
 {	
+	const size_t n = 10;
+	int* mass = new int[n] {5, 3, 2, 4, 1, 6, 11, 0 ,-1, -2};
+	msort(mass, n);
+
+	for (size_t i = 0; i < n; i++)
+	{
+		cout << mass[i] << endl;
+	}
 	return 0;
 }
 
