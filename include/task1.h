77a1c288a084405ef2bb484bd97d4ccba0b7fce9
Stanislav Stoianov <stanis.stoyanov@gmail.com>
2020-11-07 17:38:35 +0300

    sorting function implemented

--- include/task1.h
+++ include/task1.h
@@ -1,7 +1,63 @@
 /*
  * @author Stanislav Stoianov
  */
+
+/*
+ * Написать шаблонную функцию msort, сортирующую массив элементов T слиянием.
+ * Если тип элемента массива char*, то производится сортировка по длине строк.
+ */
+
 #ifndef TASK1_TASK1_H
 #define TASK1_TASK1_H
+
+#include <iostream>
+#include <cassert>
+#include <vector>
+#include <cstdlib>
+
+using namespace std;
+
+template<class T>
+void MergeSortWorker(vector<T> &a, vector<T> &x, int s, int e) {
+    if (e - s < 1) return;
+    int m = (s + e) / 2;
+    MergeSortWorker<T>(a, x, s, m);
+    MergeSortWorker<T>(a, x, m + 1, e);
+    int i = s, j = m + 1, k = 0;
+    while (i <= m && j <= e) {
+        if (a[i] < a[j]) {
+            x[k] = a[i];
+            i++;
+            k++;
+        } else {
+            x[k] = a[j];
+            j++;
+            k++;
+        }
+    }
+    while (i <= m) {
+        x[k] = a[i];
+        i++;
+        k++;
+    }
+    while (j <= e) {
+        x[k] = a[j];
+        j++;
+        k++;
+    }
+    for (i = s; i <= e; i++) a[i] = x[i - s];
+}
+
+template<class T>
+void msort(vector<T> &v) {
+    vector<T> x(v.size());
+    MergeSortWorker<T>(v, x, 0, v.size() - 1);
+}
+
+template<class T>
+bool Sorting(vector<T> &a) {
+    for (int i = 0; i < a.size() - 1; ++i) if (a[i] > a[i + 1]) return false;
+    return true;
+}
 
 #endif
--- src/main1.cpp
+++ src/main1.cpp
@@ -1,3 +1,34 @@
 /*
  * @author Stanislav Stoianov
  */
+
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include "../include/task1.h"
+
+using namespace std;
+
+int main() {
+    vector<int> a;
+    a.reserve(1000);
+    for (int i = 0; i < 1000; ++i) a.push_back(-50 + rand() % 100);
+    msort(a);
+    printf("\n%d", Sorting(a));
+
+    vector<int> b;
+    b.reserve(1001);
+    for (int i = 0; i < 1001; ++i) b.push_back(-50 + rand() % 100);
+    msort(b);
+    printf("\n%d", Sorting(b));
+
+    vector<int> c;
+    c.push_back(2);
+    msort(c);
+    printf("\n%d", Sorting(c));
+
+    vector<int> d;
+    for (int i = 1; i < 6; i++) d.push_back(i);
+    msort(d);
+    printf("\n%d", Sorting(d));
+}
