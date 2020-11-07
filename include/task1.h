c2d980f086dad85058252175e6326e1972fc9023
tndumarevskaya <63363543+tndumarevskaya@users.noreply.github.com>
2020-11-07 20:32:33 +0300

    fix

--- include/task1.h
+++ include/task1.h
@@ -6,16 +6,34 @@
 #include <cstring>
 using namespace std;
 
+template<typename T>
+bool comp(T a, T b)
+{
+	if (a > b)
+		return 0;
+	else
+		return 1;
+}
+
+template <>
+bool comp(char* a, char* b)
+{
+	if (strlen(a) > strlen(b))
+		return 0;
+	else
+		return 1;
+}
+
 template<typename T> 
-void msort(T* arr, int first, int last)
+void msort(T* arr, int last, int first = 0)
 {
 	if (first >= last)
 		return;
 	int mid = first + (last - first) / 2;
-	msort(arr, first, mid);
-	msort(arr, mid + 1, last);
+	msort(arr, mid, first);
+	msort(arr, last, mid + 1);
 
-	T* buf = (T*)malloc(sizeof(T) * (last - first + 1));
+	T* buf = new T[last + 1];
 
 	int k = 0;
 	int i = first;
@@ -23,7 +41,7 @@ void msort(T* arr, int first, int last)
 
 	while (k < last - first + 1)
 	{
-		if ((j > last) || ((i <= mid) && (arr[i] < arr[j])))
+		if ((j > last) || ((i <= mid) && (comp(arr[i], arr[j]))))
 		{
 			buf[k] = arr[i];
 			k++;
@@ -36,52 +54,10 @@ void msort(T* arr, int first, int last)
 			j++;
 		}
 	}
-
 	
 	for (int h = 0; h < last - first + 1; h++)
 		arr[first + h] = buf[h];
 
-	free(buf);
-}
-	
-template<> 
-void msort<char*>(char** arr, int first, int last) 
-{
-	if (first >= last)
-		return;
-	int mid = first + (last - first) / 2;
-	msort(arr, first, mid);
-	msort(arr, mid + 1, last);
-
-	char** buf = (char**)malloc(sizeof(char*) * (last - first + 1));
-	for (int h = 0; h < last - first + 1; h++)
-		buf[h] = (char*)malloc(sizeof(char));
-
-	int s = 0;
-	int i = first;
-	int j = mid + 1;
-
-	while (s < last - first + 1)
-	{
-		if ((j > last) || ((i <= mid) && (strlen(arr[i]) < strlen(arr[j]))))
-		{
-			buf[s] = arr[i];
-			s++;
-			i++;
-		}
-		else
-		{
-			buf[s] = arr[j];
-			s++;
-			j++;
-		}
-	}
-
-
-	for (int h = 0; h < last - first + 1; h++)
-		arr[first + h] = buf[h];
-
-	for (int h = 0; h < last - first + 1; h++)
-		free(buf);
+	delete[] buf;
 }
 #endif
