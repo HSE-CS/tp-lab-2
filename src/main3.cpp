dd9fba7ef90260529c39107007a6e778378dd4d8
Nina Titova <nititova@edu.hse.ru>
2020-11-07 17:23:54 +0300

    Task3 b111

---
+++ src/main3.cpp
@@ -0,0 +1,27 @@
+#include "task3.h"
+
+template<typename T>
+T change(T a) {
+
+    return a*2;
+
+}
+
+int main() {
+
+    const int size = 5;
+
+    int arr[size]{ 5, 17, 9, 3, 4 };
+
+    for (int i = 0; i < size; i++)
+        std::cout << arr[i] << " ";
+    std::cout << std::endl;
+
+    map(arr, change);
+
+    for (int i = 0; i < size; i++)
+        std::cout << arr[i] << " ";
+    std::cout << std::endl;
+
+    return 0;
+}
