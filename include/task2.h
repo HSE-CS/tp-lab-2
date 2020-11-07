417b89aaf1e6f594fc016cad7a757c8dd5213e87
danek0100 <71225718+danek0100@users.noreply.github.com>
2020-11-08 00:16:39 +0300

    Task 2 + Task 3

--- include/task2.h
+++ include/task2.h
@@ -1,8 +1,19 @@
 #ifndef TASK2_H
 #define TASK2_H
 #define _CRT_SECURE_NO_WARNINGS
+#include <string>
+#include <iostream>
+#include <cstring>
 
 
+
+template <typename T, const size_t n> T* createArr(T (*fun)())
+{
+	T* new_arr = new T[n];
+	for (size_t i = 0; i < n; i++)
+		new_arr[i] = fun();
+	return new_arr;
+}
 
 #endif
 
--- src/main3.cpp
+++ src/main3.cpp
@@ -1,12 +1,22 @@
 #include "task3.h"
 #define _CRT_SECURE_NO_WARNINGS
 
-
-
+int change(int a);
+using namespace std;
 
 int main()
 {
+	const size_t n = 10;
+	int arr[n]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	map<int, n>(arr, change);
+
+	for (size_t i = 0; i < n; i++)
+		cout << arr[i] << ' ';
 	return 0;
 }
 
 
+int change(int a)
+{
+	return (a * 2);
+}
