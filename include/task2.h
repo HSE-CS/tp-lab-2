d3f614e26dae3026cb76b1d37fb87cd48b07f325
AlexShi23 <alieksandr_shirokov@list.ru>
2020-11-07 16:51:37 +0300

    Fix task 2

--- include/task2.h
+++ include/task2.h
@@ -2,8 +2,15 @@
 
 using namespace std;
 
+template<typename T>
+T gen()
+{
+	int t = rand();
+	return t;
+}
+
 template<typename T, int N>
-T* createArr(T(*gen))
+T* createArr(T(*gen)())
 {
 	T* arr = new T[N];
 	for (int i = 0; i < N; i++)
--- src/main2.cpp
+++ src/main2.cpp
@@ -3,17 +3,11 @@
 
 using namespace std;
 
-template<class T>
-T gen()
+int main() 
 {
-	int t = rand();
-	return t;
-}
-
-int main() {
 	const int n = 7;
 	int* arr = createArr<int, n>(gen);
 	for (int i = 0; i < n; ++i) 
 		cout << arr[i] << endl;
-	return 0;
+	return 0; 
 }
