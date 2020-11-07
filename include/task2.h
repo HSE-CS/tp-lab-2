86f79b3adbf36fb083394b2d2ba986b23c697c3d
Mushka Nikita <namushka@edu.hse.ru>
2020-11-07 16:45:30 +0300

    fix 2

--- include/task2.h
+++ include/task2.h
@@ -2,7 +2,7 @@
 template<typename T, size_t size>
 T* createArr(T(*gen)()) {
     T* arr = new T[size];
-    for (size_t i = 0; i < size; i++)
+    for (int i = 0; i < size; i++)
         arr[i] = gen();
     return arr;
 }
