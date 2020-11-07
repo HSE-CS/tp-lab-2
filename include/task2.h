7c7ff2c14a65d5b5a796d1d058f96461dae130ca
GlebNeshchetkin <glebneshchetkin@gmail.com>
2020-11-07 17:26:50 +0300

    Test

--- include/task2.h
+++ include/task2.h
@@ -10,11 +10,11 @@
 #include <cstdlib>
 #include <ctime>
 //using namespace std;
-template<class T, size_t N>
-T* createArr(T(*f)())
+template<typename T, size_t N>
+T* createArr(T(*gen)())
 {
     T* arr = new T[N];
-    for (size_t i = 0; i < N; ++i) arr[i] = f();
+    for (size_t i = 0; i < N; ++i) arr[i] = gen();
     return arr;
 }
 #endif
