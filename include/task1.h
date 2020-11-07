de65dfd4ed193839def62e31c14003adae4da08b
Nina Titova <nititova@edu.hse.ru>
2020-11-07 17:33:32 +0300

    Task1 b111

--- include/task1.h
+++ include/task1.h
@@ -12,7 +12,6 @@ bool compare(T el1, T el2) {
 
 template < > bool compare<char*>(char* el1, char* el2) {
 
-	//return strcmp(el1, el2);
 	return strlen(el1) < strlen(el2);
 
 }
@@ -73,99 +72,6 @@ void msort(T* arr, size_t size) {
 
 	}
 
-	/*if (left < right) {
-
-		msort(arr, left, (left + right) / 2);
-		msort(arr, ((left + right) / 2) + 1, right);
-
-	}
-
-	long mid = 0;
-	long p1 = 0;
-	long p2 = 0;
-
-	T* temp = new T[100];
-
-	mid = (left + right) / 2;
-	p1 = left;
-	p2 = mid + 1;
-	for (long i = left; i <= right; i++) {
-
-		if ((p1 <= mid) && ((p2 > right) || (compare(arr[p1], arr[p2]) > 0))) {
-
-			temp[i] = arr[p1];
-			p1++;
-
-		}
-		else {
-
-			temp[i] = arr[p2];
-			p2++;
-
-		}
-
-	}
-
-	for (long i = left; i <= right; i++)
-		arr[i] = temp[i];
-
-	delete[] temp;*/
-
-	//return arr;
-
 } 
-
-
-/*
-
-if (N > 1)
-	{
-		size_t center = N / 2;
-
-		T* arr_1 = new T[center];
-		T* arr_2 = new T[N - center];
-
-		for (int i = 0; i < center; i++)
-			arr_1[i] = arr[i];
-		for (size_t i = 0; i < N - center; i++)
-			arr_2[i] = arr[i + center];
-
-		msort(arr_1, center);
-		msort(arr_2, N - center);
-
-		int  k = 0, i = 0, j = 0;
-
-		while (i < center && j < N - center)
-		{
-			if (compare(arr_1[i], arr_2[j]))
-			{
-				arr[k] = arr_1[i];
-				i++;
-			}
-			else
-			{
-				arr[k] = arr_2[j];
-				j++;
-			}
-			k++;
-
-		}
-
-		while (i < center)
-		{
-			arr[k] = arr_1[i];
-			i++;
-			k++;
-		}
-		while (j < N - center)
-		{
-			arr[k] = arr_2[j];
-			j++;
-			k++;
-		}
-
-	}
-
-*/
 
 #endif
--- src/main1.cpp
+++ src/main1.cpp
@@ -4,10 +4,7 @@ int main() {
 
 	const unsigned int size = 5;
 
-	//int arr[size] = { 35, 13, 7, 54, 9, 102, 30, 22, 11, 10 };
-
 	int arr[size] = { 35, 13, 7, 54, 9 };
-	//const char* str[] = { "aaaa", "aa", "aaaaa", "a", "aaa" };
 	char* str[size]{ (char*)"aaaa", (char*)"aa", (char*)"aaaaa", (char*)"a", (char*)"aaa" };
 
 	for (int i = 0; i < size; i++)
