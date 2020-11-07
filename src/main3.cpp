b64655789d6b6c4321fdfefd23af084ca08a3785
Stanislav Stoianov <stanis.stoyanov@gmail.com>
2020-11-07 18:00:07 +0300

    map function implemented

--- src/main3.cpp
+++ src/main3.cpp
@@ -1,3 +1,17 @@
 /*
  * @author Stanislav Stoianov
  */
+
+#include <iostream>
+#include "../include/task3.h"
+
+int doubling(int a);
+
+int main() {
+    int arr[]{1, 2, 3, 4, 5};
+    map<int, 5>(arr, doubling);
+    for (auto num: arr) std::cout << num << " ";
+    return 0;
+}
+
+int doubling(int a) { return 2 * a; }
