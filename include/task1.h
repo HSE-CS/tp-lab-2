d620930a50a614f8f487a9b75025d3a377970f83
burgundypie <kotyonok2001@mai.ru>
2020-11-08 22:41:47 +0300

    Симонова Арина

---
+++ include/task1.h
@@ -0,0 +1,81 @@
+#ifndef TASK1_H
+#define TASK1_H
+
+#include <cstring>
+
+using namespace std;
+
+
+template <typename T> void MergeSort(T* a, int left, int right, int size)
+{
+	if (left == right)
+		return;
+
+	int mid = (left + right + 1) / 2;
+
+	MergeSort(a, left, mid - 1, size);
+	MergeSort(a, mid, right, size);
+
+	T* arr = new T[size]{ 0 };
+	int j = 0;
+	int start = left;
+	int k = mid;
+	for (j = 0; j < right - left + 1; j++)
+	{
+		if ((k > right) || ((start <= mid - 1) && (a[start] < a[k])))
+		{
+			arr[j] = a[start];
+			start += 1;
+		}
+		else
+		{
+			arr[j] = a[k];
+			k += 1;
+		}
+	}
+	for (j = 0; j < right - left + 1; j++)
+	{
+		a[left + j] = arr[j];
+	}
+	delete[] arr;
+}
+
+template <> void MergeSort(char** a, int left, int right, int size)
+{
+	if (left == right)
+		return;
+
+	int mid = (left + right + 1) / 2;
+
+	MergeSort(a, left, mid - 1, size);
+	MergeSort(a, mid, right, size);
+
+	char** arr = new char* [size];
+	int j = 0;
+	int start = left;
+	int f = mid;
+	for (j = 0; j < right - left + 1; j++)
+	{
+		if ((f > right) || ((start <= mid - 1) && (strlen(a[start]) < strlen(a[f]))))
+		{
+			arr[j] = a[start];
+			start += 1;
+		}
+		else
+		{
+			arr[j] = a[f];
+			f += 1;
+		}
+	}
+	for (j = 0; j < right - left + 1; j++)
+	{
+		a[left + j] = arr[j];
+	}
+}
+
+template <typename T> void msort(T* arr, int size)
+{
+	MergeSort(arr, 0, size - 1, size);
+}
+
+#endif TASK1_H
---
+++ src/main1.cpp
@@ -0,0 +1,25 @@
+#include "task1.h"
+#include <iostream>
+
+int main()
+{
+	const size_t n = 4;
+	int* arr1 = new int[n] {4, 3, 1, 2};
+	msort(arr1, n);
+	for (size_t i = 0; i < n; i++)
+		cout << arr1[i] << ' ';
+	cout << endl;
+	delete[] arr1;
+
+	double arr2[n]{ 4.0, 3.0, 2, 1 };
+	msort(arr2, n);
+	for (size_t i = 0; i < n; i++)
+		cout << arr2[i] << " ";
+	cout << endl;
+
+	char* arr3[n]{ (char*)"aaaa", (char*)"aaa", (char*)"a", (char*)"aa" };
+	msort(arr3, n);
+	for (size_t i = 0; i < n; i++)
+		cout << arr3[i] << "\n";
+	cout << endl;
+}
