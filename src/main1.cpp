819952eba111a69c71e2a1e6b503dc215923b2aa
Дима булатов <dabulatov@edu.hse.ru>
2020-11-08 21:05:17 +0300

    Maybe 1 more example?

--- src/main1.cpp
+++ src/main1.cpp
@@ -5,6 +5,14 @@
 using namespace std;
 
 int main() {
+
+    int a[]{ 3, 1, 5, 13, 4, 2, 56, 865 };
+    msort(a, 8);
+    for (auto i : a)
+        cout << i << " ";
+    cout << "\n";
+
+
     char *c[5] = {(char*)"Lol", (char*)"M", (char*)"Mohe", (char*)"Loqurt", (char*)"Nympy"};
     msort(c, 5);
     for (auto i : c)
