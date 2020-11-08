1ba97c1a2aafbe0a4ae418b6f5818c8affa93ca0
TolimanStaR <toliman.st4r@gmail.com>
2020-11-08 23:12:06 +0300

    required file

---
+++ include/task1.h
@@ -0,0 +1,10 @@
+//
+// Created by toliman on 08.11.2020.
+//
+
+#ifndef TASK1_TASK1_H
+#define TASK1_TASK1_H
+
+
+
+#endif //TASK1_TASK1_H
---
+++ src/main1.cpp
@@ -0,0 +1,12 @@
+//
+// Created by toliman on 08.11.2020.
+//
+
+#include "task1.h"
+
+
+signed main() {
+
+
+    return 0;
+}
