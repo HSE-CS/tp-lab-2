32c987a7a4585e69e20cd016deefd08b4cd0dca2
Shaarlotka <tytylik14@gmail.com>
2020-11-08 19:42:57 +0300

    task3 fixed

--- include/task3.h
+++ include/task3.h
@@ -5,7 +5,7 @@ using namespace std;
 
 
 template <typename T, size_t size> 
-void map(T* array[size], T (*change)()){
+void map(T* array, T (*change)(T)){
 	for (int i = 0; i < size; i++){
 		array[i] = change(array[i]);
     }
