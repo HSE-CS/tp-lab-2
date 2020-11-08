ccae8406bc2eb5a11e25330e40c50924f4d1eae7
__dolt_coder__ <ihackit595@gmail.com>
2020-11-08 18:15:42 +0300

    finish second task

---
+++ include/task2.h
@@ -0,0 +1,9 @@
+template<typename T, int size>
+T* createArr(T (*gen)())  {
+    T* arr = new T[size];
+    for(int i = 0; i < size;i++) {
+        arr[i] = gen();
+    }
+    return arr;
+}
+
---
+++ src/main2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cstring>
+#include "../include/task2.h"
+
+int main() {
+    const size_t n = 5;
+	int* arr;
+	arr = createArr<int, n>(gen);
+    return 0;
+}
