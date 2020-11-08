9158087245177720058a6b17c030d7aaf81ca4a3
Right-bugor <innokentiyyyyy@gmail.com>
2020-11-08 23:12:05 +0300

    m2

--- src/main2.cpp
+++ src/main2.cpp
@@ -2,6 +2,12 @@
 #include "task2.h"
 using namespace std;
 
+int gen() {
+	static int a = 0;
+	++a;
+	return a;
+}
+
 int main() {
 	auto a = createArr<int, 10>(gen);
 	for (size_t i = 0; i < 10; ++i) {
