24d9727a0c8c91d90775411d925aec61086ede8c
__dolt_coder__ <ihackit595@gmail.com>
2020-11-08 20:43:00 +0300

    add showcase of functions

--- src/main2.cpp
+++ src/main2.cpp
@@ -2,6 +2,19 @@
 #include <cstring>
 #include "../include/task2.h"
 
+int genFunc();
+
 int main() {
+    const int size = 4;
+	int* arr;
+	arr = createArr<int, size>(genFunc);
+    for(size_t i = 0; i < size; i++)
+            std::cout<<arr[i]<<" ";
     return 0;
+}
+
+int genFunc()
+{
+    static int t = 48;
+    return t++;
 }
