c3df199c1f3b122356d0b729cf5062238876ecc9
AlexeusC4ever <62253233+AlexeusC4ever@users.noreply.github.com>
2020-11-08 11:43:03 +0300

    Update task1.h

--- include/task1.h
+++ include/task1.h
@@ -2,122 +2,52 @@
 #include <iostream>
 #include <cstring>
 
-class BUF {
-public: 
-	int length;
-	int index;
-};
 
-template <typename T>
-T* merge(T* m1, T* m2, int l1, int l2) {
-	T* ret = new T[l1 + l2];
-	int n = 0;
-
-	while (l1 && l2) {
-		if (*m1 < *m2) {
-			ret[n] = *m1;
-			m1++; l1--;
-		}
-		else {
-			ret[n] = *m2;
-			m2++; l2--;
-		}
-		n++;
-	}
-
-	if (l1 == 0) {
-		for (int i = 0; i < l2; i++) {
-			ret[n++] = *m2++;
-		}
-	}
-
-	else {
-		for (int i = 0; i < l1; i++) {
-			ret[n++] = *m1++;
-		}
-	}
-	return ret;
+template<class T>
+bool comparator(T a, T b) {
+	return (a < b);
 }
 
-template<> BUF* merge(BUF* m1, BUF* m2, int l1, int l2) {
-	BUF* ret = new BUF[l1 + l2];
-	int n = 0;
-
-	while (l1 && l2) {
-		if ((*m1).length < (*m2).length) {
-			ret[n] = *m1;
-			m1++; l1--;
-		}
-		else {
-			ret[n] = *m2;
-			m2++; l2--;
-		}
-		n++;
-	}
-
-	if (l1 == 0) {
-		for (int i = 0; i < l2; i++) {
-			ret[n++] = *m2++;
-		}
-	}
-
-	else {
-		for (int i = 0; i < l1; i++) {
-			ret[n++] = *m1++;
-		}
-	}
-	return ret;
+template<> bool comparator<char*>(char* a, char* b) {
+	return strlen(a) < strlen(b);
 }
 
-
-template <typename T>
-void msort(T* mas, int len) {
-	int n = 1, l, ost;
-	T* mas1;
-	while (n < len) {
-		l = 0;
-		while (l < len) {
-			if (l + n >= len) break;
-			ost = (l + n * 2 > len) ? (len - (l + n)) : n;
-			mas1 = merge(mas + l, mas + l + n, n, ost);
-			for (int i = 0; i < n + ost; i++) mas[l + i] = mas1[i];
-			delete[] mas1;
-			l += n * 2;
+template<class T>
+void msort(T* a, int n) {
+	if (n > 1) {
+		int firsthalf = n / 2;
+		int secondhalf = n - firsthalf;
+		T* new_a = new T[firsthalf];
+		for (int i = 0; i < firsthalf; i++) {
+			new_a[i] = a[i];
 		}
-		n *= 2;
-	}
-}
-
-template<> void msort(char** mas, int len) {
-	BUF* buf = (BUF*)malloc(sizeof(BUF) * len);
-	for (int i = 0; i < len; ++i) {
-		buf[i].length = strlen(mas[i]);
-		buf[i].index = i;
-	}
-	int n = 1, l, ost;
-	BUF* mas1;
-	while (n < len) {
-		l = 0;
-		while (l < len) {
-			if (l + n >= len) break;
-			ost = (l + n * 2 > len) ? (len - (l + n)) : n;
-			mas1 = merge(buf + l, buf + l + n, n, ost);
-			for (int i = 0; i < n + ost; i++) buf[l + i] = mas1[i];
-			delete[] mas1;
-			l += n * 2;
+		msort(new_a, firsthalf);
+		T* new_r = new T[secondhalf];
+		for (int i = 0; i < secondhalf; i++) {
+			new_r[i] = a[i + firsthalf];
 		}
-		n *= 2;
+		msort(new_r, secondhalf);
+		int firstind = 0, secondind = 0;
+		for (int i = 0; i < n; i++) {
+			if (firstind < firsthalf && secondind < secondhalf) {
+				if (comparator(new_a[firstind], new_r[secondind])) {
+					a[i] = new_a[firstind];
+					firstind++;
+				}
+				else {
+					a[i] = new_r[secondind];
+					secondind++;
+				}
+			}
+			else if (firstind == firsthalf) {
+				a[i] = new_r[secondind];
+				new_r++;
+			}
+			else if (secondind == secondhalf) {
+				a[i] = new_a[firstind];
+				new_a++;
+			}
+		}
 	}
-	/*for (int i = 0; i < 2; ++i) {
-		std::cout << buf[i].length << "\n";
-	}*/
-	char** mas2 = (char**)malloc(len * sizeof(char));
-	for (int i = 0; i < len; ++i) {
-		mas2[i] = (char*)malloc(strlen((mas[buf[i].index]) + 1) * sizeof(char));
-		strcpy(mas2[i], mas[buf[i].index]);
-	}
-	for (int i = 0; i < len; ++i) {
-		mas[i] = (char*)calloc(strlen(mas2[i]) + 1, sizeof(char));
-		strcpy(mas[i], mas2[i]);
-	}
+
 }
