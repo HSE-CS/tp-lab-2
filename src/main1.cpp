1b27be0fe0374a620162578a36d1f3af89e1eb3d
Дима булатов <dabulatov@edu.hse.ru>
2020-11-08 21:00:37 +0300

    ok, nice trying

--- src/main1.cpp
+++ src/main1.cpp
@@ -1,17 +1,13 @@
 ﻿#include <iostream>
 #include <cstring>
-
 #include "task1.h"
 
 using namespace std;
 
 int main() {
-    const size_t n = 5;
-    char *arr[5] = {(char*)"Lol", (char*)"M", (char*)"Mohe", (char*)"Loqurt", (char*)"Nympy"};
-    msort(arr, n);
-    for (size_t i = 0; i < n; ++i)
-        cout << arr[i] << '\t';
-    cout << endl;
-
+    char *c[5] = {(char*)"Lol", (char*)"M", (char*)"Mohe", (char*)"Loqurt", (char*)"Nympy"};
+    msort(c, 5);
+    for (auto i : c)
+        cout << i << " ";
     return 0;
 }
