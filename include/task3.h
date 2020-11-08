89119e9487dbb57294e0eda9e044e8ca2429e2bf
alexgiving <alexgiving@mail.ru>
2020-11-08 13:38:40 +0300

    task3_test1

---
+++ include/task3.h
@@ -0,0 +1,19 @@
+//
+// Created by Алексей Трутнев on 07.11.2020.
+//
+
+#ifndef UNTITLED11_TASK3_H
+#define UNTITLED11_TASK3_H
+
+template<class T, size_t size>
+void map(T arr[], T (*change)(T)) {
+    for (size_t i = {0}; i < size; ++i)
+        arr[i] = {change(arr[i])};
+}
+
+template<class T>
+int change(T a) {
+    return a + a;
+}
+
+#endif //UNTITLED11_TASK3_H
