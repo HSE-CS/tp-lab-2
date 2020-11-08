0f08cb5c6f7f6f26fe4ca7176ae8c37e3a472fbf
Gerasimov Alexey <gerasimov2122@icloud.com>
2020-11-08 23:10:16 +0300

    another one.....

--- include/task1.h
+++ include/task1.h
@@ -29,7 +29,7 @@ void merge(char** arr, int l, int mid, int r) {
     int p1 = l;
     int p2 = mid + 1;
     int p3 = 0;
-    T *temp = new char*[r - l + 1];
+    char** temp = new char*[r - l + 1];
     while (p1 <= mid && p2 <= r) {
         if (strlen(arr[p1]) < strlen(arr[p2]))
             temp[p3++] = arr[p1++];
