ef590c70432341435ca7931fe2a6717d7b8ffdb0
burgundypie <kotyonok2001@mai.ru>
2020-11-08 22:46:22 +0300

    Симонова Арина

--- include/task2.h
+++ include/task2.h
@@ -1,21 +1,16 @@
 #ifndef TASK2_H
 #define TASK2_H
+
 #include <iostream>
-#include <cstdlib>
 
-int genUpTo10()
-{
-	int num = rand() % 10;
-	return num;
-};
+using namespace std;
 
-template <typename T, size_t N>
-T* createArr(T(*gen)())
+template<typename T, size_t size> T* createArr(T(*gen)())
 {
-	T* newArray = new T[N];
-	for (size_t i = 0; i < N; i++)
-		newArray[i] = gen();
-	return newArray;
-};
+	T* arr = new T[size];
+	for (int i = 0; i < size; i++)
+		arr[i] = gen();
+	return arr;
+}
 
 #endif
--- src/main2.cpp
+++ src/main2.cpp
@@ -1,11 +1,19 @@
 #include "task2.h"
+#include <iostream>
+
+template<class T>
+T gen() {
+	static int t = 48;
+	return t++;
+}
 
 int main()
 {
-	const size_t arraySize = 10;
-	int* array = createArr<int, arraySize>(genUpTo10);
-	for (size_t i = 0; i < arraySize; i++)
-		std::cout << array[i] << " ";
-	std::cout << std::endl;
+	const size_t n = 4;
+	int* arr;
+	arr = createArr<int, n>(gen);
+	for (size_t i = 0; i < n; i++) {
+		cout << arr[i] << endl;
+	}
 	return 0;
 }
