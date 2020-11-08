a449eab4b4e8147f965df4719c62253b1f264e42
burgundypie <kotyonok2001@mai.ru>
2020-11-08 21:49:47 +0300

    Симонова Арина

--- include/task3.h
+++ include/task3.h
@@ -1,5 +1,13 @@
-template<typename T, int size>
-void map(T arr[], T(*change)(T)) {
-	for (int i = 0; i < size; i++)
+#ifndef TASK3_H
+#define TASK3_H
+
+#include<iostream>
+
+using namespace std;
+
+template<typename T, size_t n>
+void map(T* arr, T(*change)(T val)) {
+	for (size_t i = 0; i < n; i++)
 		arr[i] = change(arr[i]);
 }
+#endif
--- src/main3.cpp
+++ src/main3.cpp
@@ -1,7 +1,18 @@
-#include <iostream>
-#include <cstring>
 #include "task3.h"
 
-int main() {
-	return 0;
+using namespace std;
+
+template<class T>
+T change(T val)
+{
+	return val + 2;
+}
+
+int main()
+{
+	const size_t n = 5;
+	int arr[n]{ 1, 2, 3, 4, 5 };
+	map<int, n>(arr, change);
+	for (size_t i = 0; i < n; i++)
+		printf("%d ", arr[i]);
 }
