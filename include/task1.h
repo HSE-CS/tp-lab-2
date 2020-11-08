b92aec9cda440ca29b0030ffc7a5f15ff15803d5
PolinaZharavina <pdzharavina@edu.hse.ru>
2020-11-08 17:59:45 +0300

    First test

--- include/task1.h
+++ include/task1.h
@@ -11,7 +11,96 @@ using namespace std;
 
 template <typename T> void msort(T* a, int n)
 {
-    
+    int mid = n / 2 + 1;
+    if (n % 2 == 1)
+        mid++;
+
+    int h = 1;
+
+    T* c = (T*)malloc(n * sizeof(T));
+    int step;
+    while (h < n)
+    {
+        step = h;
+        int i = 0;
+        int j = mid;
+        int k = 0;
+        while (step <= mid)
+        {
+            while ((i < step) && (j < n) && (j < (mid + step)))
+            {
+                if (a[i] < a[j])
+                {
+                    c[k] = a[i];
+                    i++; k++;
+                }
+                else {
+                    c[k] = a[j];
+                    j++; k++;
+                }
+            }
+            while (i < step)
+            {
+                c[k] = a[i];
+                i++; k++;
+            }
+            while ((j < (mid + step)) && (j < n))
+            {
+                c[k] = a[j];
+                j++; k++;
+            }
+            step = step + h;
+        }
+        h = h * 2;
+        for (i = 0; i < n; i++)
+            a[i] = c[i];
+    }
 }
 
-#endif 
+template<> void msort<>(char** a, int n)
+{
+    int mid = (n + n % 4) / 2;
+    if (n % 2 == 1)
+        mid++;
+
+    int h = 1;
+    char** c = (char**)malloc(n * sizeof(char*));
+    int step;
+    while (h < n)
+    {
+        step = h;
+        int i = 0;
+        int j = mid;
+        int k = 0;
+        while (step <= mid)
+        {
+            while ((i < step) && (j < n) && (j < (mid + step)))
+            {
+                if (strlen(a[i]) < strlen(a[j]))
+                {
+                    c[k] = a[i];
+                    i++; k++;
+                }
+                else {
+                    c[k] = a[j];
+                    j++; k++;
+                }
+            }
+            while (i < step)
+            {
+                c[k] = a[i];
+                i++; k++;
+            }
+            while ((j < (mid + step)) && (j < n))
+            {
+                c[k] = a[j];
+                j++; k++;
+            }
+            step = step + h;
+        }
+        h = h * 2;
+        for (i = 0; i < n; i++)
+            a[i] = c[i];
+    }
+}
+#endif  
