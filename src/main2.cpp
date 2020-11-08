ee268cb597c6b27a053301d1021c65b2b79deb24
alexgiving <alexgiving@mail.ru>
2020-11-08 13:19:41 +0300

    task1_test1

---
+++ src/main2.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Алексей Трутнев on 07.11.2020.
+//
+#include<iostream>
+#include "task2.h"
+
+int main() {
+    const size_t n = {5};
+    int* arr = createArr<int, n>(gen);
+    for (int i = {0}; i < n; ++i)
+        std::cout << arr[i]<<" ";
+    return 0;
+}
