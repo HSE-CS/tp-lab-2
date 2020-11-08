4df08667ee77a3db5bed80fb36a2b45e1960a615
Дима булатов <dabulatov@edu.hse.ru>
2020-11-08 21:40:20 +0300

    -random

--- include/task2.h
+++ include/task2.h
@@ -1,20 +1,16 @@
 #ifndef TASK2_H
 #define TASK2_H
 
-#include <random>
 #include<iostream>
+
 using namespace std;
 
-int gen() {
-    random_device r;
-    uniform_int_distribution<int> r2(-5, 5);
-    return r2(r);
-}
-
-template<class T, size_t N>
-T* createArr(T(*f)()) {
-    T* arr = new T[N];
-    for (size_t i = 0; i < N; ++i) arr[i] = f();
+template<typename T, size_t n>
+T* createArr(T(*gen)()) {
+    T* arr = new T[n];
+    for (size_t i = 0; i < n; i++)
+        arr[i] = gen();
     return arr;
 }
+
 #endif
--- src/main2.cpp
+++ src/main2.cpp
@@ -3,8 +3,16 @@
 
 using namespace std;
 
+
+template<class T>
+T gen()
+{
+    static int t = 50;
+    return --t;
+}
+
 int main() {
-    const size_t n = { 5 };
+    const size_t n = 5;
     int* a = createArr<int, n>(gen);
     for (int i = 0; i < n; ++i) {
         cout << a[i] << ' ';
