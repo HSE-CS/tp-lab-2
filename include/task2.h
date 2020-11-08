97435f41b0889c707980a48841fd222d1aaa3e19
Alexander Grachev <s89519011368@yandex.ru>
2020-11-08 19:27:33 +0300

    task2 2

--- include/task2.h
+++ include/task2.h
@@ -5,7 +5,7 @@
 
 template<typename T, unsigned int N >
 
-T* createArr(T*(gen)()){
+T* createArr(T(*gen)()){
     T* arr = new T[N];
     for (unsigned int i = 0; i < N; i++)
         arr[i] = gen();
