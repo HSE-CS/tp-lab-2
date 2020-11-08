55d18b7456835f0bb284ac41585ab69349c9336e
Дима булатов <dabulatov@edu.hse.ru>
2020-11-08 21:13:40 +0300

    One more example?

--- src/main1.cpp
+++ src/main1.cpp
@@ -1,17 +1,21 @@
 ﻿#include <iostream>
-#include <cstring>
 #include "task1.h"
 
 using namespace std;
 
 int main() {
 
-    int a[]{ 3, 1, 5, 13, 4, 2, 56, 865 };
+    int a[]{ 5, 3, 2, 3, 10, 22, 6, 85 };
     msort(a, 8);
     for (auto i : a)
         cout << i << " ";
     cout << "\n";
 
+    double b[]{ 3.5, 1.1, 5.6, 13.5, 4.1, 10.0 };
+    msort(b, 6);
+    for (auto i : b)
+        cout << i << " ";
+    cout << "\n";
 
     char *c[5] = {(char*)"Lol", (char*)"M", (char*)"Mohe", (char*)"Loqurt", (char*)"Nympy"};
     msort(c, 5);
