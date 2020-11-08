fede88b9a46d939f543d8c663aac4da5593641a0
__dolt_coder__ <ihackit595@gmail.com>
2020-11-08 20:47:58 +0300

    fix array initialization in main1

--- src/main1.cpp
+++ src/main1.cpp
@@ -3,8 +3,8 @@
 #include "../include/task1.h"
 
 int main() {
-    int size = 5;
-	double arr[size]{18.3, 98.3, -15.2, 0, 8800.555};
+    const int size = 5;
+	double arr[size] = {18.3, 98.3, -15.2, 0, 8800.555};
     msort(arr, size);
     for(size_t i = 0; i < size; i++)
         std::cout<<arr[i]<<" ";
