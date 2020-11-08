1e16d4c988bd043eea68031a6da2f0506ad75a0e
Karina Mashanova <karmen.01@inbox.ru>
2020-11-08 22:15:52 +0300

    First

--- include/task2.h
+++ include/task2.h
@@ -1,19 +1,7 @@
 #define task2_h
 #include <iostream>
 
-template<class T> T gen() {
-	static int t = 48;
-	return t++;
-}
 
-template<> char* gen() {
-	static unsigned t = 0;
-	t++;
-	char* s = new char[t + 1];
-	for (size_t i = 0; i < t; i++) s[i] = '0' + i;
-	s[t] = '\0';
-	return s;
-}
 
 template<typename T, size_t N>
 T* createArr(T(*gen)())
--- include/task3.h
+++ include/task3.h
@@ -2,17 +2,6 @@
 #include <iostream>
 
 
-template<class T> T change(T val) {
-	return val + 1;
-}
-
-template<> char* change(char* val){
-	size_t n = strlen(val);
-	for (size_t i = 0; i < n; i++)
-		val[i] = val[i] + 1;
-	return val;
-}
-
 
 template<typename T, size_t N> void map(T* arr, T(*change)(T val)){
 	for (size_t i = 0; i < N; ++i) {
