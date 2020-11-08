62ad0cf68c41b54ca705fc02c2c31a0352168703
alexgiving <alexgiving@mail.ru>
2020-11-08 13:36:03 +0300

    task2_test1

---
+++ include/task2.h
@@ -0,0 +1,25 @@
+//
+// Created by Алексей Трутнев on 07.11.2020.
+//
+
+#ifndef UNTITLED11_TASK2_H
+#define UNTITLED11_TASK2_H
+
+#include<iostream>
+#include <random>
+
+template<class T, size_t size>
+T* createArr(T (*gen)() ){
+    T* arr = new T[size];
+    for (size_t i = {0}; i < size; ++i)
+        arr[i] = {gen()};
+    return arr;
+}
+
+int gen() {
+    std::random_device rd;
+    std::uniform_int_distribution<int> uid(-50, 50);
+    return uid(rd);
+}
+
+#endif //UNTITLED11_TASK2_H
