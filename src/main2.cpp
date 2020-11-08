1e04f4d7dad8e5063cb7923ee99b846a2f0befd2
Дима булатов <dabulatov@edu.hse.ru>
2020-11-08 21:34:18 +0300

    mistake in ex2?

--- src/main2.cpp
+++ src/main2.cpp
@@ -1,12 +1,14 @@
 ﻿#include "task2.h"
 #include <iostream>
-#define N 10
 
 using namespace std;
 
 int main() {
-    int* a = createArr<int, N>(gen);
-    for (int i = 0; i < N; ++i) cout << a[i] << ' ';
+    const size_t n = { 5 };
+    int* a = createArr<int, n>(gen);
+    for (int i = 0; i < n; ++i) {
+        cout << a[i] << ' ';
+    }
     cout << '\n';
     return 0;
 }
