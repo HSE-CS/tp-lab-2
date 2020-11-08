8a5daec168794e6bb20cdc7bd13ff6d4fc1e5e30
Nik <50590478+stifeevn@users.noreply.github.com>
2020-11-08 11:37:41 +0500

    add main2.cpp

---
+++ src/main2.cpp
@@ -0,0 +1,16 @@
+#include "task2.h"
+
+template<class T>
+T gen() {
+    return 0;
+}
+
+int main() {
+    const size_t n = 5;
+    int* arr;
+    arr = createArr<int, n>(&gen);
+    for (size_t i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
