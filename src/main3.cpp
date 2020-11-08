c5fef1c0066617b80a5572b31526bc33a5d17b2c
burgundypie <kotyonok2001@mai.ru>
2020-11-08 22:51:25 +0300

    Симонова Арина

--- src/main3.cpp
+++ src/main3.cpp
@@ -1,12 +1,19 @@
 #include "task3.h"
+#include <iostream>
+
+template<class T>
+T change(T val)
+{
+	return val + 1;
+}
 
 int main()
 {
-	const size_t arraySize = 10;
-	int array[arraySize] = { 1, 2, 0, 9, 4, 7, 1, 0, 8, 3 };
-	map<int, arraySize>(array, changeMod2);
-	for (size_t i = 0; i < arraySize; i++)
-		std::cout << array[i] << " ";
-	std::cout << std::endl;
+	const size_t n = 4;
+	int arr[n]{ 10, 20, 30, 40 };
+	map<int, n>(arr, change);
+	for (int i = 0; i < n; i++) {
+		cout << arr[i] << endl;
+	}
 	return 0;
 }
