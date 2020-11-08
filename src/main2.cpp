d0d903cc649694f0046ddb332257d3e644dad7cf
EgorBuzanov <egor4ek007@gmail.com>
2020-11-08 19:24:38 +0300

    Egor Buzanov

--- src/main2.cpp
+++ src/main2.cpp
@@ -5,6 +5,6 @@ int main()
     const size_t arraySize = 10;
     int *array = createArr<int, arraySize>(gen);
     for (size_t i = 0; i < arraySize; i++)
-        std::cout << array[i] / 100 << " ";
+        std::cout << array[i] << " ";
     return 0;
 }
