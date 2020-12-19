ff578df19a6a7ff5259aa593cbb96a37756a1f0c
alex-linyx1 <alextasenko25@gmail.ru>
2020-12-19 18:00:36 +0300

    sending lab 2 again

---
+++ include/task2.h
@@ -0,0 +1,10 @@
+#pragma once
+
+
+template < typename T, int N >
+T* createArr(T(*gen)())
+{
+	T * x = new T [N];
+	for (int i = 0; i < N; i++) { x[i] = gen(x[i]); }
+	return x;
+}
---
+++ src/main1.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include"task1.h"
+////
+
+
+int main()
+{
+	return 0;
+}
